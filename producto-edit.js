f6051054a93c69acd56c6ddea1a8462ba5cbb972
crpb90 <106489696+crpb90@users.noreply.github.com>
2023-07-06 00:53:03 -0300

    Update producto-edit.js

--- producto-edit.js
+++ producto-edit.js
@@ -11,7 +11,7 @@ createApp({
       imagen: "",
       stock: 0,
       precio: 0,
-      url: "http://Crod23.pythonanywhere.com/productos/" + id,
+      url: "https://Crod23.pythonanywhere.com/productos/" + id,
     };
   },
   methods: {
